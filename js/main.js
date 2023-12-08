8dcf564bd40ed5d09eabb76d224e36fe8a369830
BuildTools <unconfigured@null.spigotmc.org>
2023-12-08 22:39:27 +0200

    New Repo!

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let flag = false;
+
+function switch_theme() {
+    if(flag) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        flag = false;
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        flag = true;
+    }
+}
+
+function cards_slide_right(event) {
+    const container = event.target.parentElement.getElementsByClassName('cards-container')[0];
+    const card = container.getElementsByClassName('card-item');
+    container.scrollLeft += card[0].offsetWidth;
+}
+function cards_slide_left(event) {
+    const container = event.target.parentElement.getElementsByClassName('cards-container')[0];
+    const card = container.getElementsByClassName('card-item');
+    container.scrollLeft -= card[0].offsetWidth;
+}
